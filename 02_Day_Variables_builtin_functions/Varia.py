4e50e282ca9585caf811fd50d61cffdc1e07035d
AMN-47 <akhilnagar57@gmail.com>
2026-07-09 19:47:56 +0200

    Completed exercise 1

---
+++ 02_Day_Variables_builtin_functions/Varia.py
@@ -0,0 +1,26 @@
+'''Exercises: Level 1
+
+Inside 30DaysOfPython create a folder called day_2. Inside this folder create a file named variables.py
+Write a python comment saying 'Day 2: 30 Days of python programming'
+Declare a first name variable and assign a value to it
+Declare a last name variable and assign a value to it
+Declare a full name variable and assign a value to it
+Declare a country variable and assign a value to it
+Declare a city variable and assign a value to it
+Declare an age variable and assign a value to it
+Declare a year variable and assign a value to it
+Declare a variable is_married and assign a value to it
+Declare a variable is_true and assign a value to it
+Declare a variable is_light_on and assign a value to it
+Declare multiple variable on one line'''
+
+first_name = "John"
+last_name = "Doe"
+full_name = "John Doe"
+country = "Colombia"
+age = 20
+year = "2025"
+is_married = True
+is_light_on = False
+
+fname, lname, county, oldness, is_taken = 'Jane', 'Doe', 'Orange', 20, False
